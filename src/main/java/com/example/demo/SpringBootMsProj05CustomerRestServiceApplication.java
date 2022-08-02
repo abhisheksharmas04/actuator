4fbf7bab2fa31ffa2276f344a36062109dbbe204
Abhishek <Abhishek@Abhishek>
2022-08-02 21:46:52 +0530

    Spring boot Actuators

---
+++ src/main/java/com/example/demo/SpringBootMsProj05CustomerRestServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMsProj05CustomerRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMsProj05CustomerRestServiceApplication.class, args);
+	}
+
+}
